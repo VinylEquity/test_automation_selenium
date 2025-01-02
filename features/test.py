5e6d16299a485bc1eb83a7a4fa1d5c773746f2b2
dilip-vinyl <dilip.kumar@vinylequity.com>
2025-01-02 15:01:09 +0530

    Added code for date picking

--- features/test.py
+++ features/test.py
@@ -1,9 +1,19 @@
-import os
+from datetime import datetime
+from pytz import timezone
+tz = timezone('EST')
+print(datetime.now(tz))
+dt = str(datetime.now(tz)).split(" ")[1].split(":")
+# print(dt[0])
+# print(dt[1])
 
-from dotenv import load_dotenv
-from selenium import webdriver
-# load_dotenv()
-# print(type(os.getenv('HOST')))
-# print(os.getenv('HOST'))
-driver = webdriver.Chrome()
-driver.get("https://sandbox.transferge.com/signin")
+# pick_mins = 5*round(int(dt[1])/5)+5
+# print(pick_mins)
+# if pick_mins > 55:
+#     dt[0] = int(dt[0])+1
+#     pick_mins = dt[1] = 0
+#
+# print([5 * i for i in range(0, 12)])
+# print([5 * i for i in range(0, 12)].index(pick_mins)+1)
+#
+# print(dt[0])
+# print(dt[1])
